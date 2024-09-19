08d3e8c66d73a3709ced2c1e10075f1b2c09a32b
nguyentppse161945 <nguyentppse161945@gmail.com>
2024-09-19 09:00:30 +0700

    nguyentpp_removeUnUsedEnpoint

--- VegaCityApp/Constants/ApiEndPointConstant.cs
+++ VegaCityApp/Constants/ApiEndPointConstant.cs
@@ -93,7 +93,6 @@ public static class ApiEndPointConstant
         public const string MomoPayment = ApiEndpoint + "/payment/momo";
         public const string ZaloPayment = ApiEndpoint + "/payment/zalo";
         public const string VnPayPayment = ApiEndpoint + "/payment/vnpay";
-        public const string VnPaymentExecute = ApiEndpoint + "/payment/vnpay";
         public const string VisaCardPayment = ApiEndpoint + "/payment/visa";
     }
 }
