8c8c712b455c2e64532b2708b271e1b9ab56dcba
nguyentppse161945 <nguyentppse161945@gmail.com>
2024-10-08 17:20:16 +0700

    nguyentpp_8Oct_adjustTimeStamp

--- VegaCityApp/Utils/TimeUtils.cs
+++ VegaCityApp/Utils/TimeUtils.cs
@@ -4,21 +4,25 @@
     {
         public static string GetTimestamp(DateTime value)
         {
-            return value.ToString("yyyyMMddHHmmssff");
+            DateTime seaTime = value.AddHours(7);
+            return seaTime.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
         }
 
-        public static string GetHoursTime(DateTime value)
-        {
-            return value.ToString("H:mm");
-        }
+        //public static string GetHoursTime(DateTime value)
+        //{
+        //    return value.ToString("H:mm");
+        //}
 
         public static DateTime GetCurrentSEATime()
         {
             //TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
-            DateTime localTime = DateTime.UtcNow;
-            //DateTime utcTime = TimeZoneInfo.ConvertTime(localTime, TimeZoneInfo.Local, tz);
-            //DateTime utcTime = localTime.AddDays(7);
-            return localTime;
+            //DateTime localTime = DateTime.UtcNow;
+            ////DateTime utcTime = TimeZoneInfo.ConvertTime(localTime, TimeZoneInfo.Local, tz);
+            ////DateTime utcTime = localTime.AddDays(7);
+            //return localTime;
+            DateTime utcTime = DateTime.UtcNow; // Get the current UTC time
+            DateTime convertedTime = utcTime.AddHours(7); // Add 7 hours for Ho Chi Minh City's time (UTC+7)
+            return convertedTime;
         }
 
         public static DateTime ConvertToSEATime(DateTime value)
@@ -26,7 +30,7 @@
             //TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
             //DateTime convertedTime = TimeZoneInfo.ConvertTime(value, tz);
 
-            //DateTime convertedTime = value.AddHours(7);
+            DateTime convertedTime = value.AddHours(7);
             return value;
         }
     }
