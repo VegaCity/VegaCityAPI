54052bfa71ee7a869a11ec729c335d6abaf6c755
dattq2002 <merycome123@gmail.com>
2024-10-11 00:32:12 +0700

    Update TimeUtils.cs

--- VegaCityApp/Utils/TimeUtils.cs
+++ VegaCityApp/Utils/TimeUtils.cs
@@ -14,7 +14,7 @@
 
         public static DateTime GetCurrentSEATime()
         {
-            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("Asia/Ho_Chi_Minh");
+            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("Asia/Ho Chi Minh");
             DateTime localTime = DateTime.UtcNow;
             DateTime utcTime = TimeZoneInfo.ConvertTime(localTime, TimeZoneInfo.Local, tz);
             //DateTime utcTime = localTime.AddDays(7);
@@ -23,7 +23,7 @@
 
         public static DateTime ConvertToSEATime(DateTime value)
         {
-            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("Asia/Ho_Chi_Minh");
+            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
             DateTime convertedTime = TimeZoneInfo.ConvertTime(value, tz);
 
            // DateTime convertedTime = value.AddHours(7);
