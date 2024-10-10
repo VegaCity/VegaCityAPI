19a983a582ed619001dcde94e1d8adea7f56a296
dattq2002 <merycome123@gmail.com>
2024-10-11 01:59:38 +0700

    Update JwtUtil.cs

--- VegaCityApp/Utils/JwtUtil.cs
+++ VegaCityApp/Utils/JwtUtil.cs
@@ -40,7 +40,7 @@ public class JwtUtil
         var expires = user.Role.Name.Equals(RoleEnum.Admin.GetDescriptionFromEnum())
             ? TimeUtils.GetCurrentSEATime().AddDays(1)
             : TimeUtils.GetCurrentSEATime().AddMinutes(60);
-        var token = new JwtSecurityToken(issuer, null, claims, notBefore: TimeUtils.GetCurrentSEATime(), expires, credentials);
+        var token = new JwtSecurityToken(issuer, null, claims, notBefore: TimeUtils.GetCurrentSEATime().AddHours(-7), expires.AddHours(-7), credentials);
         return jwtHandler.WriteToken(token);
     }
     public static string GenerateRefreshToken(User user, Tuple<string, Guid> guidClaim, DateTime? expireDay)
@@ -60,8 +60,8 @@ public class JwtUtil
             new Claim(ClaimTypes.Role, user.Role.Name),
         };
         if (guidClaim != null) claims.Add(new Claim(guidClaim.Item1, guidClaim.Item2.ToString()));
-        var expires = expireDay;
-        var token = new JwtSecurityToken(issuer, null, claims, notBefore: TimeUtils.GetCurrentSEATime(), expires, credentials);
+        DateTime expires = (DateTime)expireDay;
+        var token = new JwtSecurityToken(issuer, null, claims, notBefore: TimeUtils.GetCurrentSEATime().AddHours(-7), expires.AddHours(-7), credentials);
         return jwtHandler.WriteToken(token);
     }
     //function decode jwt token to get expire date
