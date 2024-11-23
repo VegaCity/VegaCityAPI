40f9e8c2976353ecd099239a89f87980477c7760
merycome123@gmail.com <merycome123@gmail.com>
2024-11-23 23:31:49 +0700

    Update StoreService.cs

--- VegaCityApp/Services/Implement/StoreService.cs
+++ VegaCityApp/Services/Implement/StoreService.cs
@@ -518,7 +518,7 @@ namespace VegaCityApp.API.Services.Implement
 
             foreach (var storeTransfer in storeMoneyTransfers)
             {
-                if (storeTransfer.Description.Split("to")[1].Trim() == "store")
+                if (storeTransfer.Description.Split(" to ")[1].Trim() == "store")
                 {
                     storeMoneyTransfersListToStore.Add(storeTransfer);
                 }
